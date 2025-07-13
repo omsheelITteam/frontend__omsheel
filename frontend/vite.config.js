16856cbe30481d2bc38627eef3bff65b790b1f4f
omsheelITteam <sunil.s@omsheelgroup.in>
2025-07-13 19:21:28 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    historyApiFallback: true, // 👈 ensures React Router routes are served correctly
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
+});
